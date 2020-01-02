60f2cb559467a2837a69c9cb60b4357fbae9bf50
charrli <charrli@126.com>
2020-01-01 23:22:22 -0800

    added active record

---
+++ chapter04/ASPPatterns.Chap4.ActiveRecord/ASPPatterns.Chap4.ActiveRecord.Model/Model.cs
@@ -0,0 +1,57 @@
+﻿using Castle.ActiveRecord;
+using Castle.ActiveRecord.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap4.ActiveRecord.Model
+{
+    [ActiveRecord("Comments")]
+    public class Comment : ActiveRecordBase<Comment>
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        [BelongsTo("PostID")]
+        public Post Post { get; set; }
+        [Property]
+        public string Text { get; set; }
+        [Property]
+        public string Author { get; set; }
+        [Property]
+        public DateTime DateAdded { get; set; }
+    }
+
+    [ActiveRecord("Posts")]
+    public class Post : ActiveRecordBase<Post>
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        [Property]
+        public string Subject { get; set; }
+        [Property]
+        public string Text { get; set; }
+        public string ShortText
+        {
+            get
+            {
+                if (Text.Length > 20)
+                    return Text.Substring(0, 20) + "...";
+                else
+                    return Text;
+            }
+        }
+        [HasMany]
+        public IList<Comment> Comments { get; set; }
+        [Property]
+        public DateTime DateAdded { get; set; }
+        public static Post FindLatestPost()
+        {
+            SimpleQuery<Post> q = new SimpleQuery<Post>
+            (@"from Post p order by p.DateAdded desc");
+            return (Post)q.Execute()[0];
+        }
+    }
+}
+
---
+++ chapter04/ASPPatterns.Chap4.ActiveRecord/ASPPatterns.Chap4.ActiveRecord.UI.MVC/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using ASPPatterns.Chap4.ActiveRecord.Model;
+using Castle.ActiveRecord.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ASPPatterns.Chap4.ActiveRecord.UI.MVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IConfigurationSource source = ConfigurationManager.GetSection("activeRecord") as IConfigurationSource;
+            Castle.ActiveRecord.ActiveRecordStarter.Initialize(source, typeof(Post), typeof(Comment));
+        }
+    }
+}
